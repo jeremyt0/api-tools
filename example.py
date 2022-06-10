8212d8dac9be6b3fc03a4c02afcde5fb0d331fd0
jeremyt0 <jez_is_the_best@hotmail.com>
2022-06-10 23:54:19 +0100

    Add basic example of running api

---
+++ example.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Helloo": "World"}
+
+@app.get("/info")
+async def read_info():
+    return {"info": "This is my fastapi 0.0.1"}
+
+
+# Run in cmd: uvicorn example:app --reload
+
+if __name__=="__main__":
+    # Run in python
+    uvicorn.run("example:app", host="127.0.0.1", port=5000, log_level="info")
